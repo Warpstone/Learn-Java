7cbfb6493d833849fc262ae779aee97c702e7ac3
Sasha <Alexsandr.Feofanov@gmail.com>
2025-10-23 23:06:11 +0300

    if-else. solution_1

--- Solution.java
+++ Solution.java
@@ -1,34 +1,26 @@
-
+// Ввести с клавиатуры три целых числа. Определить, есть ли среди них хотя бы одна пара равных между собой чисел.
+// Если такая пара существует, вывести на экран числа через пробел. Если все три числа равны между собой, то вывести все три.
 
 import java.util.Scanner;
 
-/* 
-Три числа
-*/
-
 public class Solution {
     public static void main(String[] args) {
-       try (Scanner console = new Scanner(System.in)) {
-		int a = console.nextInt();
-        int b = console.nextInt();
-        int c = console.nextInt();
-        
-        Boolean chi = (a == b);
-        Boolean chis = (b == c);
-        Boolean chisl = (a == c);
-        
-        if (chi)
-        	System.out.println(a + " " + b);
-        else
-        	System.out.println(a);
-            if (chis)
-            	System.out.println(b + " " + c);           
-            if (chisl)
-                System.out.println(a + " " + c);
-                else
-                System.out.println(c);
-            
-        
-	   }
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	int number1 = scanner.nextInt();
+    	int number2 = scanner.nextInt();
+    	int number3 = scanner.nextInt();
+    	
+    	if (number1 == number2) { // Сначала мы проверяем условие, если все 3 числа раны между собой, то выводим на экран все три числа
+    		if (number1 == number3) {
+    			System.out.println(number1 + " " + number2 + " " + number3);
+    		} else {
+    			System.out.println(number1 + " " + number2); // Иначе (все три числа не равны) выводи равные пары чисел
+    		}
+    	} else if (number1 == number3) {
+    		System.out.println(number1 + " " + number3);
+    	} else if (number2 == number3) {
+    		System.out.println(number2 + " " + number3);
+    	}
     }
 }
