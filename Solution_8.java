e569df8ba5a05958ef6914c39d5f176db50b69b9
Sasha <Alexsandr.Feofanov@gmail.com>
2025-10-29 23:13:17 +0300

    if-else + ternarn operator. Solution_8

---
+++ Solution_8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution_8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number < 5) {
+            System.out.println("число меньше 5");
+        } else if (number > 5) {
+            System.out.println("число больше 5");
+        } else if (number == 5){
+            System.out.println("число равно 5");
+        }
+        
+        //String result = number < 5 ? "число меньше 5" : number > 5 ? "число больше 5" : "число равно 5";
+        //System.out.println(result);
+    }
+}
