d2db8fcfc37519f7c4897b7c64112cadb139f10a
Sasha <Alexsandr.Feofanov@gmail.com>
2025-10-29 23:03:02 +0300

    ternarn operator. Solution_6

---
+++ Solution_7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution_7 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		int number1 = scanner.nextInt();
+		int number2 = scanner.nextInt();
+		System.out.println(number1 > number2 ? number1 : number2);
+
+	}
+
+}
