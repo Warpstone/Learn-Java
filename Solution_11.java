9e94fa24ef8f883cbd341c24612b1bccfee45235
Sasha <Alexsandr.Feofanov@gmail.com>
2025-10-31 23:48:24 +0300

    if-else.equals(Solution_11)

---
+++ Solution_11.java
@@ -0,0 +1,28 @@
+public class Solution_11 {
+	
+    public static String string1 = "Амиго";
+    public static String string2 = string1;
+    public static String string3 = new String(string1);
+
+public static void main(String[] args) {
+        String same = "ссылки на строки одинаковые";
+        String different = "ссылки на строки разные";
+        if (string1 == string2) {
+            System.out.println(same);
+        } else {
+            System.out.println(different);
+        }
+
+        if (string2 == string3) {
+            System.out.println(same);
+        } else {
+            System.out.println(different);
+        }
+
+        if (string1 == string3) {
+            System.out.println(same);
+        } else {
+            System.out.println(different);
+        }
+    }
+}
