b7db34776c5a7db0ba71c4a57606e2840f688b1a
Sasha <Alexsandr.Feofanov@gmail.com>
2025-11-17 20:28:28 +0700

    start learn while. Solution_12

---
+++ Solution_12.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution_12 {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner (System.in);
+		while (console.hasNextInt()) {
+			int x = console.nextInt();
+		}
+
+	}
+
+}
