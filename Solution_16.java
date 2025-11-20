b3d7195c1b91f01dcef9f68eeb02f6e922789e99
Sasha <Alexsandr.Feofanov@gmail.com>
2025-11-20 20:48:17 +0700

    begin learn loop while. Solution_16

---
+++ Solution_16.java
@@ -0,0 +1,19 @@
+
+public class Solution_16 {
+
+	public static void main(String[] args) {
+		int n = 0;
+		while (n < 4) {
+			int m = 0; {
+				while (m < 5) {
+					System.out.print("A");
+					m++;
+				}
+				System.out.println();
+				n++;
+			}
+		}
+
+	}
+
+}
