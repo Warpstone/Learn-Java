eedc0a5c394952138a9fa82a1ff71ce4c42f321d
Sasha <Alexsandr.Feofanov@gmail.com>
2025-11-21 23:58:41 +0700

    Begin learn loop while. Solution_18(16)

---
+++ Solution_18.java
@@ -0,0 +1,18 @@
+// Похожая задача решается в файле Solution_16
+public class Solution_18 {
+
+	public static void main(String[] args) {
+		int n = 0;
+		while (n < 5) {
+			int m = 0;
+			while (m < 10) {
+				System.out.print("Q");
+				m++;
+			}
+			System.out.println();
+			n++;
+		}
+
+	}
+
+}
