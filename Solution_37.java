9d8120fc35e59e464ee3bfabe0afdbfa57b133dd
Sasha <Alexsandr.Feofanov@gmail.com>
2025-12-21 12:21:35 +0700

    Solution_37.От 1 до 100

---
+++ Solution_37.java
@@ -0,0 +1,14 @@
+/*
+ * Программа которая выводит числа от 1 до 100
+ */
+
+public class Solution_37 {
+
+	public static void main(String[] args) {
+		for (int a = 1; a < 101; a++) {
+			System.out.println(a);
+		}
+
+	}
+
+}
