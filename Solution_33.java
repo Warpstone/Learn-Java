612d4a081e3ed2385a90870ad74dfa2891e4bbfc
Sasha <Alexsandr.Feofanov@gmail.com>
2025-12-24 00:03:34 +0700

    Solution_40 + правки по задаче _33

--- Solution_33.java
+++ Solution_33.java
@@ -1,13 +1,16 @@
+/*
+ * Написать программу которая выводит 10 строк и выводит на экран сколько из этих строк было чисел.
+ */
 import java.util.Scanner;
 public class Solution_33 {
 
 	public static void main(String[] args) {
-		Scanner console = new Scanner(System.in);
-		int count = 0;
-		for(int i = 0; i < 10; i++) {
-			if (console.hasNextInt())
-				count++;
-			console.nextLine();
+		Scanner console = new Scanner(System.in);		// Создаем объект Scanner для чтения данных из консоли.
+		int count = 0;									// В этой переменной будет хранить кол-во чисел.
+		for(int i = 0; i < 10; i++) {					// Цикл от 0 до 10 (не включая 10).
+			if (console.hasNextInt())					// Если в консоль ввели число,
+				count++;								// то увеличиваем на единицу.
+			console.nextLine();							// Считываем строку из консоли и никуда ее не сохраняем.
 		}
 		System.out.println(count);
 	}
