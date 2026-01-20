42173b6122ce8f7de48a9fa547e359a6ce7469ac
Sasha <Alexsandr.Feofanov@gmail.com>
2026-01-20 20:47:59 +0700

    Пример работы типа char;

---
+++ Project_51.java
@@ -0,0 +1,12 @@
+/*
+	Пример применения переменных char;
+ */
+public class Project_51 {
+	static void main() {
+		char ch1, ch2;
+		ch1 = 88;                               // Код для Х;
+		ch2 = 'Y';
+		System.out.print("ch1 и ch2: ");
+		System.out.println(ch1 + " " + ch2);
+	}
+}
