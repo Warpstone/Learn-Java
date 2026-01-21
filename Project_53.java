ddc4fedf7b67bd8e5f806bd5339018773bf88d6c
Sasha <Alexsandr.Feofanov@gmail.com>
2026-01-21 18:23:20 +0700

    Пример работы типа boolean;

---
+++ Project_53.java
@@ -0,0 +1,23 @@
+/*
+	Пример работы типа boolean;
+ */
+public class Project_53 {
+	static void main() {
+
+		boolean b;
+
+		b = false;
+		System.out.println("b равно " + b);
+
+		b = true;
+		System.out.println("b равно " + b);
+
+		if(b) System.out.println("Это выполняется.");
+
+		b = false;
+		if(b) System.out.println("Это не выполняется.");
+
+		System.out.println("10 > 9 равно " + (10 > 9));
+
+	}
+}
