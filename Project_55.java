240f87ec496c0beb336a6ffffb1908b497c35948
Sasha <Alexsandr.Feofanov@gmail.com>
2026-01-22 10:41:04 +0700

    Пример работы блока кода;

---
+++ Project_55.java
@@ -0,0 +1,14 @@
+/*
+	Демонстрация работы блока кода;
+ */
+public class Project_55 {
+	static void main() {
+			int x;
+			int y = 20;
+			for(x = 0; x < 10; x++) {
+				System.out.println("Значение х: " + x);
+				System.out.println("Значение y: " + y);
+				y = y - 2;
+			}
+	}
+}
