3ad21aeb742cda52dc2cf1a5646ac02ef0b90dec
Sasha <Alexsandr.Feofanov@gmail.com>
2026-01-26 21:42:43 +0700

    Задача со звездочками; Редактирование;

--- Project_59.java
+++ Project_59.java
@@ -1,10 +1,10 @@
 public class Project_59 {
 	static void main() {
-		for (int i = 1; i <= 5; i++) {
-			for (int j = 1; j <= 5; j++) {
+		for (int i = 1; i <= 5; i++) {                  // Условие;
+			for (int j = 2; j <= i; j++) {              // Пока выполняется верхнее условие, выполняется и внутреннее условие;
 				System.out.print("*");
 			}
-			System.out.println();
+			System.out.println();                       // Первый круг цикла завершился, новая строка;
 		}
 	}
 }
