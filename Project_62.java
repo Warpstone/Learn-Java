e7f4798e45bca62aabb1b1edc8c5e7d96822349c
Sasha <Alexsandr.Feofanov@gmail.com>
2026-02-08 18:19:04 +0700

    Еще один простой пример применения оператора switch;

---
+++ Project_62.java
@@ -0,0 +1,25 @@
+public class Project_62 {
+	static void main() {
+		for (int i = 1; i < 12; i++) {
+			switch (i) {
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+					System.out.println("i меньше 5");
+					break;
+				case 5:
+				case 6:
+				case 7:
+				case 8:
+				case 9:
+					System.out.println("i меньше 10");
+					break;
+				default:
+					System.out.println("i больше или равно 10");
+			}
+
+		}
+	}
+}
