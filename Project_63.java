df96121b1bdba6baf03dc12437984d9fce3ae402
Sasha <Alexsandr.Feofanov@gmail.com>
2026-02-09 19:29:33 +0700

    Пример математическими выражениями.

---
+++ Project_63.java
@@ -0,0 +1,17 @@
+public class Project_63 {
+	static void main() {
+		int a = 10000;
+		int b = 50;
+		int c = 12;
+
+		int x = a * c;
+		int y = x - (b * c);
+		int z = y;
+
+		if (z > 10000) {
+			System.out.println("AAA");
+		} else  {
+			System.out.println("BBB");
+		}
+	}
+}
