e61ad69cb42f3bc24cee7ea8284a67f72ecc867b
Sasha <Alexsandr.Feofanov@gmail.com>
2026-02-20 22:31:30 +0700

    Еще один пример со switch-case и работа со строками;

---
+++ Project_70.java
@@ -0,0 +1,18 @@
+public class Project_70 {
+	static void main() {
+		String username = "John";
+
+		switch (username) {
+			case "Doe":
+				System.out.println("Username is Doe");
+				break;
+			case "John":
+				System.out.println("Username is John");
+				break;
+			case "Jane":
+				System.out.println("Username is Jane");
+				break;
+			// Username is John
+		}
+	}
+}
