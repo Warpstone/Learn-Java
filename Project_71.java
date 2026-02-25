85512bc9430ddadf4947f4f1d1c6c51149403e0a
Sasha <Alexsandr.Feofanov@gmail.com>
2026-02-25 21:39:29 +0700

    Example with loop While + nested loop While.

---
+++ Project_71.java
@@ -0,0 +1,17 @@
+public class Project_71 {
+	static void main() {
+		int m = 0;
+		int n = 0;
+
+		while (m < 10) {
+			System.out.println(m);
+			m++;
+
+			while (n < 10) {
+					System.out.println("You can everythink");
+					n++;
+				}
+
+		}
+	}
+}
