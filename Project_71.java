f27999e5147dcee7d29e480769bed56a7fe712eb
Sasha <Alexsandr.Feofanov@gmail.com>
2026-02-26 20:15:38 +0700

    Simple example loop for.

--- Project_71.java
+++ Project_71.java
@@ -3,15 +3,9 @@ public class Project_71 {
 		int m = 0;
 		int n = 0;
 
-		while (m < 10) {
+		while (m <= n) {
 			System.out.println(m);
 			m++;
-
-			while (n < 10) {
-					System.out.println("You can everythink");
-					n++;
-				}
-
 		}
 	}
 }
---
+++ Project_72.java
@@ -0,0 +1,7 @@
+public class Project_72 {
+	static void main() {
+		for (int m = 1; m < 100; m++) {
+			System.out.println(m);
+		}
+	}
+}
