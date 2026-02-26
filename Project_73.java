fbbceed4d4c7a0e39a932c123759640ad1303489
Sasha <Alexsandr.Feofanov@gmail.com>
2026-02-26 20:41:05 +0700

    Simple example Random.

---
+++ Project_73.java
@@ -0,0 +1,7 @@
+public class Project_73 {
+	static void main() {
+		for (int i = 0; i < 25; i++) {
+
+		}
+	}
+}
---
+++ Project_74.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Project_74 {
+	static void main() {
+		// Создание генератора случайных чисел.
+		Random rand = new Random(25);
+		int i, j, k;
+		j = rand.nextInt(25);
+		System.out.println("j : " + j);
+		k = rand.nextInt(25);
+		System.out.println("k : " + k);
+	}
+}
