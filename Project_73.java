44262a346b5123fdbdb3b96bfd54b1b74defc6a6
Sasha <Alexsandr.Feofanov@gmail.com>
2026-03-01 21:14:46 +0700

    Simple example for String.

--- Project_73.java
+++ Project_73.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Project_73 {
 	static void main() {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = 0;
 		for (int i = 0; i < 25; i++) {
 
 		}
---
+++ Project_75.java
@@ -0,0 +1,12 @@
+public class Project_75 {
+	static void main() {
+		String s1 = "Hey";
+		String s2 = "Java";
+		String s3 = s1.concat(s2);
+
+		System.out.println(s3);
+		System.out.println(s1.charAt(0));
+		System.out.println(s2.length());
+		System.out.println(s3.equals(s2));
+	}
+}
